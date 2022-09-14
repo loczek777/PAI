a27d1a48fa5b27b938e33ddadff4f5623a7f3618
loczek777 <leagueoffpoland@interia.pl>
2022-09-14 11:44:54 +0200

    strona

--- html/strona.php
+++ html/strona.php
@@ -12,9 +12,43 @@ echo "Hello World <br> ";
 
 echo "My counter 1 <br>";
 
-echo "Hello Friend";
+echo "Hello Friend <br>";
 ?>
+
+<?php
+
+$msgs = 0;
+echo "Masz ". $msgs ." wiadomosci. ";
+
+if ($msgs == 0) {
+	echo "brak wiadomosci";
+}
+else {
+	echo "Masz ". $msgs ." wiadomosci. ";
+}
+//?>
+<?php
+//x=0
+//y=0
+//++$x
+//$y ++
+//if(x<=10) {
+//	echo x;
+//}
+//?>
+
+<?php
+//
+//x=10
+//if(x+x==10) echo x
+//y=0
+//if(y++==0) echo y 
+
+
+//?>
 </body>
+
+
 
 
 </html>
