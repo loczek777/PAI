62b8fc9f6e4c482e89feeec6b74ec768709bbb2f
loczek777 <leagueoffpoland@interia.pl>
2022-09-21 11:42:53 +0200

    liczby

---
+++ petle/liczby.php
@@ -0,0 +1,42 @@
+<?php
+
+
+/* $procenty = 100;
+
+if ($procenty <=39){
+echo "masz 1";
+}
+else if($procenty >=40 && $procenty <=54) {
+	echo " masz 2 " ;
+}
+else if($procenty >=55 && $procenty <=69) {
+	echo " masz 3 " ;
+}
+
+else if($procenty >=70 && $procenty <=84) {
+	echo " masz 4" ;
+}
+
+else if($procenty >=85 && $procenty <=94) {
+	echo " masz 5 " ;
+}
+
+else if($procenty >=95 && $procenty <=100) {
+	echo " masz 6 " ;
+}
+*/
+
+
+
+
+
+
+
+?>
+<?php
+for($i=-3; $i<=3; $i++){
+	if($i==0) continue;
+	echo $i ."<br>";
+	
+ }
+ ?>
